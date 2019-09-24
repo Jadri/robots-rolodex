58ac0271b66d3b93e92988f5ba54ece4193ff6b2
Rijad Bešlija <rijadb10@gmail.com>
2019-09-24 23:30:35 +0200

    Updates

---
+++ precache-manifest.94a38e2545e118dd23989cb57cc94a08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50524f7bafc14b9e4b77579718a532a6",
+    "url": "/robot-rolodex/index.html"
+  },
+  {
+    "revision": "9e5778fa131a93d2699f",
+    "url": "/robot-rolodex/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "6e1e12b9856f32550a2e",
+    "url": "/robot-rolodex/static/css/main.e49e4299.chunk.css"
+  },
+  {
+    "revision": "9e5778fa131a93d2699f",
+    "url": "/robot-rolodex/static/js/2.925ef8d5.chunk.js"
+  },
+  {
+    "revision": "6e1e12b9856f32550a2e",
+    "url": "/robot-rolodex/static/js/main.9a04987d.chunk.js"
+  },
+  {
+    "revision": "3b2acadd47b2938b3b84",
+    "url": "/robot-rolodex/static/js/runtime-main.997b563b.js"
+  }
+]);
